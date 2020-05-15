3b4df0e2b92f3730e270203791aabd877caf9b1d
root <root@localhost>
2020-05-15 17:13:07 +0800

    first

---
+++ release/CMakeFiles/stereo_kitti.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dell/catkin_ws/src/ORB_SLAM2/Examples/Stereo/stereo_kitti.cc" "/home/dell/catkin_ws/src/ORB_SLAM2/release/CMakeFiles/stereo_kitti.dir/Examples/Stereo/stereo_kitti.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  "/usr/local/include/eigen3"
+  "/usr/local/lib/cmake/Pangolin/../../../include"
+  "/usr/include/eigen3"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev/opencv"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/dell/catkin_ws/src/ORB_SLAM2/release/CMakeFiles/ORB_SLAM2.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
